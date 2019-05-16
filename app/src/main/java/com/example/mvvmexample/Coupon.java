3e7b419516ecefb0880944fe37617ffb3f466706
samirdangal <samirdangal@gmail.com>
2019-05-16 15:11:25 +0545

    add cupon pojo class and coupon demo data

---
+++ app/src/main/java/com/example/mvvmexample/Coupon.java
@@ -0,0 +1,35 @@
+package com.example.mvvmexample;
+
+public class Coupon {
+
+    String store;
+    String offer;
+    String expiry;
+
+    public Coupon() {
+    }
+
+    public String getStore() {
+        return store;
+    }
+
+    public void setStore(String store) {
+        this.store = store;
+    }
+
+    public String getOffer() {
+        return offer;
+    }
+
+    public void setOffer(String offer) {
+        this.offer = offer;
+    }
+
+    public String getExpiry() {
+        return expiry;
+    }
+
+    public void setExpiry(String expiry) {
+        this.expiry = expiry;
+    }
+}
--- app/src/main/java/com/example/mvvmexample/MainActivity.java
+++ app/src/main/java/com/example/mvvmexample/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.mvvmexample;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    @NonNull
+    private ListView categoriesLst;
+    @NonNull
+    private ListView couponsLst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        categoriesLst = findViewById(R.id.category_list);
+        couponsLst = findViewById(R.id.coupon_list);
+
+
     }
     
 }
